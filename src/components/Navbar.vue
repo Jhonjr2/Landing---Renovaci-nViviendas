f05adfaa6bb3e0235b3ef69682fa6e2029dfdeb6
jr2jhon <rangeljhon55@gmail.com>
2025-07-25 02:12:37 -0500

    Primer commit limpio

---
+++ src/components/Navbar.vue
@@ -0,0 +1,229 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+import { useColorMode } from "@vueuse/core";
+const mode = useColorMode();
+mode.value = "light";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+
+import { ChevronsDown, Menu } from "lucide-vue-next";
+import ToggleTheme from "./ToggleTheme.vue";
+import WhatsAppIcon from "@/icons/WhatsAppIcon.vue";
+
+interface RouteProps {
+  href: string;
+  label: string;
+}
+
+interface FeatureProps {
+  title: string;
+  description: string;
+}
+
+const routeList: RouteProps[] = [
+  {
+    href: "#servicios",
+    label: "Servicios",
+  },
+  {
+    href: "#proyectos",
+    label: "Proyectos",
+  },
+  {
+    href: "#contact",
+    label: "Contacto",
+  },
+  {
+    href: "#faq",
+    label: "FAQ",
+  },
+];
+
+const featureList: FeatureProps[] = [
+ {
+   title: "Diseño Personalizado",
+   description: "Creamos espacios únicos que reflejan tu estilo y se adaptan perfectamente a tus necesidades.",
+ },
+ {
+   title: "Calidad Garantizada",
+   description: "Trabajamos con materiales premium y profesionales certificados para resultados duraderos.",
+ },
+ {
+   title: "Proyecto Completo",
+   description: "Desde el diseño 3D hasta la entrega final, nos encargamos de cada detalle de tu renovación.",
+ },
+];
+
+const isOpen = ref<boolean>(false);
+</script>
+
+<template>
+  <header
+    :class="{
+      'shadow-light': mode === 'light',
+      'shadow-dark': mode === 'dark',
+      'w-[90%] md:w-[70%] lg:w-[75%] lg:max-w-screen-xl top-5 mx-auto sticky border z-40 rounded-2xl flex justify-between items-center p-2 bg-card shadow-md': true,
+    }"
+  >
+  <a href="/" class="logo" style="display: flex; align-items: center; gap: 8px;">
+  <img src="../assets/img/logo1.ico" alt="Logo"
+       style="height: 40px; width: 40px; border-radius: 8px; object-fit: cover;" />
+  <span style="font-weight: bold;" :class="mode === 'light' ? 'text-transparent bg-gradient-to-r from-[#1A8ACC] to-[#9C27B0] bg-clip-text' : 'text-transparent bg-gradient-to-r from-[#1A8ACC] to-white bg-clip-text'"
+  >Viviendas</span>
+</a>
+
+    <!-- Mobile -->
+    <div class="flex items-center lg:hidden">
+      <Sheet v-model:open="isOpen">
+        <SheetTrigger as-child>
+          <Menu
+            @click="isOpen = true"
+            class="cursor-pointer"
+          />
+        </SheetTrigger>
+
+        <SheetContent
+          side="left"
+          class="flex flex-col justify-between rounded-tr-2xl rounded-br-2xl bg-card"
+        >
+          <div>
+            <SheetHeader class="mb-4 ml-4">
+              <SheetTitle class="flex items-center">
+                <a
+                  href="/"
+                  class="flex items-center"
+                >
+                  <ChevronsDown
+                    class="bg-gradient-to-tr from-primary/70 via-primary to-primary/70 rounded-lg size-9 mr-2 border text-white"
+                  />
+                  Viviendas
+                </a>
+              </SheetTitle>
+            </SheetHeader>
+
+            <div class="flex flex-col gap-2">
+              <Button
+                v-for="{ href, label } in routeList"
+                :key="label"
+                as-child
+                variant="ghost"
+                class="justify-start text-base"
+              >
+                <a
+                  @click="isOpen = false"
+                  :href="href"
+                >
+                  {{ label }}
+                </a>
+              </Button>
+            </div>
+          </div>
+
+          <SheetFooter class="flex-col sm:flex-col justify-start items-start">
+            <Separator class="mb-2" />
+
+            <ToggleTheme />
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
+    </div>
+
+    <!-- Desktop -->
+    <NavigationMenu class="hidden lg:block">
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger class="bg-card text-base">
+            Soluciones
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <div class="grid w-[600px] grid-cols-2 gap-5 p-4">
+              <img
+                src="../assets/img/img4.jpg"
+                alt="Beach"
+                class="h-full w-full rounded-md object-cover"
+              />
+              <ul class="flex flex-col gap-2">
+                <li
+                  v-for="{ title, description } in featureList"
+                  :key="title"
+                  class="rounded-md p-2 text-sm hover:bg-muted"
+                >
+                  <p class="mb-1 font-semibold leading-none text-foreground">
+                    {{ title }}
+                  </p>
+                  <p class="text-muted-foreground leading-relaxed">
+                    {{ description }}
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Button
+              v-for="{ href, label } in routeList"
+              :key="label"
+              as-child
+              variant="ghost"
+              class="justify-start text-base"
+            >
+              <a :href="href">
+                {{ label }}
+              </a>
+            </Button>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+
+    <div class="hidden lg:flex">
+      <ToggleTheme />
+
+      <Button
+        as-child
+        size="sm"
+        variant="ghost"
+        aria-label="View on WhatsApp"
+      >
+        <a
+          aria-label="View on WhatsApp"
+          href="https://wa.me/573116164386?text=Hola%2C%20estoy%20interesado%20en%20renovar%20mi%20vivienda%20y%20quisiera%20más%20información."
+          target="_blank"
+        >
+          <WhatsAppIcon class="size-5" />
+        </a>
+      </Button>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.shadow-light {
+  box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.085);
+}
+
+.shadow-dark {
+  box-shadow: inset 0 0 5px rgba(255, 255, 255, 0.141);
+}
+</style>
---
+++ src/components/Services.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+enum ProService {
+  YES = 1,
+  NO = 0,
+}
+
+interface ServiceProps {
+  title: string;
+  pro: ProService;
+  description: string;
+}
+
+const serviceList: ServiceProps[] = [
+  {
+    title: "Diseño Personalizado",
+    description:
+      "Creamos espacios únicos que reflejan tu estilo y necesidades, desde la idea inicial hasta el último detalle.",
+    pro: 1,
+  },
+  {
+    title: "Asesoría en Materiales",
+    description:
+      "Te guiamos en la elección de materiales de alta calidad, funcionales y estéticos para cada espacio.",
+    pro: 0,
+  },
+  {
+    title: "Remodelaciones Integrales",
+    description:
+      "Transformamos cocinas, baños y espacios completos con soluciones eficientes y modernas.",
+    pro: 1,
+  },
+  {
+    title: "Gestión de Proyectos",
+    description:
+      "Nos encargamos de todo el proceso de renovación para que tú no tengas que preocuparte por nada.",
+    pro: 0,
+  },
+];
+
+</script>
+
+<template>
+  <section id="servicios" class="container py-24 sm:py-32">
+    <h2 class="text-lg text-primary text-center mb-2 tracking-wider">
+      Servicios
+    </h2>
+
+    <h2 class="text-3xl md:text-4xl text-center font-bold mb-4">
+      Mejora tu hogar con expertos
+    </h2>
+
+    <h3 class="md:w-1/2 mx-auto text-xl text-center text-muted-foreground mb-8">
+      Soluciones personalizadas en diseño, remodelación y acabados para renovar tu espacio y aumentar el valor de tu
+      propiedad.
+    </h3>
+
+    <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-4"></div>
+
+    <div class="grid sm:grid-cols-2 lg:grid-cols-2 gap-4 w-full lg:w-[60%] mx-auto">
+      <div v-for="{ title, description } in serviceList" :key="title">
+        <Card class="bg-muted/60 dark:bg-card h-full relative">
+          <CardHeader>
+            <CardTitle>{{ title }}</CardTitle>
+            <CardDescription>{{ description }}</CardDescription>
+          </CardHeader>
+        </Card>
+      </div>
+    </div>
+  </section>
+</template>
