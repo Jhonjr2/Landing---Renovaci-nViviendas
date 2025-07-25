a6caf0152c07c5e5175b81a684f05da143447706
jr2jhon <rangeljhon55@gmail.com>
2025-07-25 12:36:19 -0500

    update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -198,7 +198,7 @@ const isOpen = ref<boolean>(false);
     </NavigationMenu>
 
     <div class="hidden lg:flex">
-      <ToggleTheme />
+      <!-- <ToggleTheme /> -->
 
       <Button
         as-child
